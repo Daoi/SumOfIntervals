2e886c963eef82a107f917fbe2880272491f2d63
Daoi <36860391+Daoi@users.noreply.github.com>
2020-02-01 19:25:56 -0500

    Initial commit

---
+++ sum_of_intervals.py
@@ -0,0 +1,16 @@
+def sum_of_intervals(intervals):
+    from collections import Counter
+    interval = []
+    length = 0
+    for i in range(len(intervals)):
+        for j in range(len(intervals[i])):
+            for x in range(intervals[i][j], intervals[i][j+1]):
+                interval.append(x)
+            break
+
+    numbers = Counter(interval)
+
+    for k in numbers:
+        length += 1
+
+    return length
